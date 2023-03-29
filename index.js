f18458828f33d8ae79114fd99255c07c6ae6444d
edd-ie <ogendoeddie@gmail.com>
2023-03-29 19:01:39 +0300

    hash map

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let hold = new Map();
+  for (let i = 0; i < array.length; i++) {
+    let difference = target - array[i];
+    if (hold.has(difference)) {
+      console.log([array[hold.get(difference)], array[i]]);
+      return true;
+    }
+    hold.set(array[i],i)
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n) time complexity
 */
 
 /* 
   Add your pseudocode here
+  create a hash map
+  loop through elements of the array
+    calculate difference between target and element
+    if the hash map has the difference
+      return the true
+    else set the current element and index to map
+  if loop completes with nothing
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  Map() store a value with its reference 
+  looping through each element we note the difference between it and the target
+  if our map has the difference, we get the value and its index and return true
+  else we map the current element and its index to them map using .set()        
+  repeat if unsuccessful return false
 */
 
 // You can run `node index.js` to view these console logs
